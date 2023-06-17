645fa2e297d4723f98939e5723568bd804316128
wildonion <ea_pain@yahoo.com>
2023-06-17 14:40:05 +0330

    change docker container name

--- redeploy.sh
+++ redeploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker stop wl-twitter-bot && docker rm wl-twitter-bot
-docker build -t wl-twitter-bot . && docker run -d -p 7999:8000 --name wl-twitter-bot --network gem wl-twitter-bot
+docker stop twidis && docker rm twidis
+docker build -t twidis . && docker run -d -p 7999:8000 --name twidis --network gem twidis
